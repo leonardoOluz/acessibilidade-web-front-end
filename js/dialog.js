0a8c04e2ee30ba358cacb88f404edfcb55ffa6f0
Leonardo Luz <leonardoluz10@hotmail.com>
2024-11-20 20:05:23 -0300

    feat: :white_check_mark: Criado evento de escuta no click da caixa modal para navegar com tab e colocar o focus no elementos.

--- js/dialog.js
+++ js/dialog.js
@@ -1,19 +1,54 @@
 // Variáveis
-var btnAbreDialog = document.querySelector('#abreDialog');
-var dialog = document.querySelector('.dialogNewsletter');
-var dialogBody = document.querySelector('.dialogNewsletter-body');
-var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
+var btnAbreDialog = document.querySelector("#abreDialog");
+var dialog = document.querySelector(".dialogNewsletter");
+var dialogBody = document.querySelector(".dialogNewsletter-body");
+var dialogOverlay = document.querySelector(".dialogNewsletter-overlay");
+const boxDialogNewsLetter = document.querySelector(".dialogNewsletter-body");
 
+btnAbreDialog.style.display = "block";
 
 // Quando abrir a dialog...
-btnAbreDialog.addEventListener('click', function() {
-  dialog.classList.add('dialogNewsletter--aberto');
+btnAbreDialog.addEventListener("click", function () {
+  dialog.classList.add("dialogNewsletter--aberto");
+  document.querySelector(".dialogNewsletter-campo").focus();
+});
+
+dialogOverlay.addEventListener("click", fechandoDialog);
+
+boxDialogNewsLetter.addEventListener("keydown", (event) => {
+  const elementBox = boxDialogNewsLetter.querySelectorAll("button, input");
+  const firstElement = elementBox[0];
+  const lastElement = elementBox[elementBox.length - 1];
+
+  if (event.key === "Tab") {
+    if (event.shiftKey) {
+      if (document.activeElement === firstElement) {
+        event.preventDefault();
+        lastElement.focus();
+      }
+    } else {
+      if (
+        document.activeElement === lastElement ||
+        !boxDialogNewsLetter.contains(document.activeElement)
+      ) {
+        event.preventDefault();
+        firstElement.focus();
+      }
+    }
+  }
+
+  if (event.key === "Escape") {
+    fechandoDialog();
+  }
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');     
+  dialog.classList.remove("dialogNewsletter--aberto");
+  btnAbreDialog.focus();
 }
 
 // Listeners
-document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
+document
+  .querySelector(".dialogNewsletter-fechar")
+  .addEventListener("click", fechandoDialog);
