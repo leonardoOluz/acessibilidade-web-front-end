3dadc8cfc167f610de51f9b2abb5049ef76da896
Leonardo Luz <leonardoluz10@hotmail.com>
2024-11-21 11:22:26 -0300

    feat: :white_check_mark: Feito o tratamento no carrosel para acessibilidade nos botões de links com javascript.

--- js/carousel.js
+++ js/carousel.js
@@ -1,15 +1,48 @@
-var btns = document.querySelectorAll('.listaDeArtigos-slider-item');
+var btns = document.querySelectorAll(".listaDeArtigos-slider-item");
+const listaNoticias = document.querySelectorAll(".listaDeArtigos-item");
+const newSliderSpanItem = document.createElement("span");
+
+newSliderSpanItem.classList.add("escondeVisualmente");
+newSliderSpanItem.textContent = "(Slide atual)";
+
+listaNoticias[0].style.display = "block";
 
 // Percorre todos os botoes controladores
-btns.forEach(function(btn) {
-  btn.addEventListener('click', function() {
-    
+btns.forEach(function (btn) {
+  // Remover o Span dos outros botoes
+  btn.querySelector(".escondeVisualmente") &&
+    btn.querySelector(".escondeVisualmente").remove();
+
+  btn.addEventListener("click", function () {
+    // btn.href = "javascript:void(0)";
+
+    listaNoticias.forEach((noticia) => {
+      noticia.style.display = "none";
+      if (
+        noticia.getAttribute("data-noticia") ===
+        btn.getAttribute("data-sliderItem")
+      ) {
+        btn.append(newSliderSpanItem);
+        noticia.style.display = "block";
+      }
+    });
 
     // Remove classe 'ativo' dos outros botoes
-    btns.forEach(function(btnRemoveClass) {
-      btnRemoveClass.classList.remove('listaDeArtigos-slider-item--ativo')
-    })
+    btns.forEach(function (btnRemoveClass) {
+      btnRemoveClass.classList.remove("listaDeArtigos-slider-item--ativo");
+    });
 
-    this.classList.add('listaDeArtigos-slider-item--ativo')
-  })
-})
+    this.classList.add("listaDeArtigos-slider-item--ativo");
+  });
+});
+
+// Percorre todas as noticias adicionando o span no botão com display block
+listaNoticias.forEach((noticia) => {
+  if (noticia.style.display === "block") {
+    btns.forEach((btn) => {
+      if (btn.getAttribute("data-sliderItem") === noticia.getAttribute("data-noticia")) {
+        btn.append(newSliderSpanItem);
+      }
+    });
+  }
+});
