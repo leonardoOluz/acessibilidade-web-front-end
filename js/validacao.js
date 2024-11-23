588516989722badaa3ae988271da0580cf53b438
Leonardo Luz <leonardoluz10@hotmail.com>
2024-11-22 21:48:29 -0300

    feat: :recycle: Refatorando arquivo validação.

--- js/validacao.js
+++ js/validacao.js
@@ -1,7 +1,8 @@
 const inputCep = document.querySelector("#cep");
 inputCep.oninvalid = () => {
-    if (!inputCep.validity.valid) {
-        inputCep.setCustomValidity("Ops, algo errado no campo CEP!");
+  inputCep.setCustomValidity("")
+  if (!inputCep.validity.valid) {
+    inputCep.setCustomValidity("Ops, algo errado no campo CEP!");
         inputCep.parentNode.classList.add("contatoCampo--erro")
-    }
+  }
 }
