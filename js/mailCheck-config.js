d72e3dd1704075050eebe805276d3ba109e068ec
Leonardo Luz <leonardoluz10@hotmail.com>
2024-11-23 20:48:40 -0300

    feat: :white_check_mark: Tratando sugestão do config no emailCheck. criando link ancora e acrescentando class na div pai.

--- js/mailCheck-config.js
+++ js/mailCheck-config.js
@@ -2,6 +2,7 @@ var domains = ["gmail.com", "uol.com"];
 var secondLevelDomains = ["hotmail"];
 var topLevelDomains = ["com", "net", "org"];
 const campoEmail = document.querySelector("#email");
+const sugestao = document.getElementById("sugestao");
 
 campoEmail.addEventListener("blur", function () {
   Mailcheck.run({
@@ -11,6 +12,9 @@ campoEmail.addEventListener("blur", function () {
     secondLevelDomains: secondLevelDomains, // optional
     suggested: function (suggestion) {
       console.log(suggestion.full)
+      sugestao.style.display = "inline-block"
+      sugestao.innerHTML = `Você quis dizer: <a href="#">${suggestion.full} ?</a>`
+      sugestao.parentNode.classList.add("suggestion.full");
     },
   });
 });
